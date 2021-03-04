ee5cba477efe3d21534bb7d8a9b0be0923446777
Jung Lee <koreanstig@gmail.com>
2021-03-03 20:28:29 -0800

    created test for employee

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,1 +1,18 @@
 const Employee = require("../lib/employee");
+
+describe("Employee class", () =>{
+    it("Can set the name, id, and email of the employee", () => {
+        const test = new Employee("David", 0, "anon@yahoo.com");
+        expect(test.name).toEqual("David");
+        expect(test.id).toEqual(0);
+        expect(test.email).toEqual("anon@yahoo.com");
+    });
+
+    describe("getRole", () => {
+        it("returns Employee", ()=>{
+            const role = "Employee";
+            const test = new Employee("David", 0, "anon@yahoo.com")
+            expect(test.getRole()).toBe(role);
+        });
+    });
+});
