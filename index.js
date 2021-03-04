76ccecf3913cb36ef35ee91856e75eeeacfdfb73
Jung Lee <koreanstig@gmail.com>
2021-03-03 18:42:52 -0800

    Added last function in index.js - to create a new html file with base html

--- index.js
+++ index.js
@@ -124,3 +124,49 @@ function employeePrompt(){
         };
     });
 };
+
+function createPage() {
+    let employeeCards = "";
+
+    employeeArray.forEach(employee => {
+        let employeeCard = employee.newEmployeeCard();
+        employeeCards += employeeCard;
+    });
+
+    let newHTML = `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap link -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Employee Summary</title>
+</head>
+
+<body>
+    <div class="container-fluid bg-danger text-center d-flex align-items-center justify-content-center" style="height: 10rem;">
+        <div>
+            <h1 style="color: white;">My Team</h1>
+        </div>
+    </div>
+
+    <div class="container" style="padding-top: 5rem;">
+        <div class="card-deck justify-content-center">
+            ${employeeCards}
+        </div>
+    </div>
+</body>
+
+</html>`;
+    fs.writeFile("index.html", newHTML, (err)=> {
+        if (err) {
+            throw err;
+        }
+    });
+};
+
+managerPrompt();
