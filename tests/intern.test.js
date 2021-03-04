5af8cec40e0418d335e17e4bf5da9ac9b66fab35
Jung Lee <koreanstig@gmail.com>
2021-03-03 20:38:38 -0800

    Added intern test

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -2,24 +2,24 @@ const Intern = require("../lib/intern");
 
 describe("Intern class", () =>{
     it("Can set the name, id, and email of the Intern", () => {
-        const test = new Intern("David", 0, "anon@yahoo.com", );
+        const test = new Intern("David", 0, "anon@yahoo.com", "UW Bootcamp");
         expect(test.name).toEqual("David");
         expect(test.id).toEqual(0);
         expect(test.email).toEqual("anon@yahoo.com");
     });
 
-    describe("getGithub", () => {
+    describe("getSchool", () => {
         it("returns the github account", ()=>{
-            const gitHub = "koreanstig";
-            const test = new Intern("David", 0, "anon@yahoo.com", "koreanstig");
-            expect(test.getGithub()).toBe(gitHub);
+            const school = "UW Bootcamp";
+            const test = new Intern("David", 0, "anon@yahoo.com", "UW Bootcamp");
+            expect(test.getSchool()).toBe(school);
         });
     });
 
     describe("getRole", () => {
-        it("returns Engineer", ()=>{
-            const role = "Engineer";
-            const test = new Intern("David", 0, "anon@yahoo.com");
+        it("returns Intern", ()=>{
+            const role = "Intern";
+            const test = new Intern("David", 0, "anon@yahoo.com", "UW Bootcamp");
             expect(test.getRole()).toBe(role);
         });
     });
